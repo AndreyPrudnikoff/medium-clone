c21216ece1efda25de2d4049cf60e05fc2e8f5c2
Andrey <a.prudniks@gmail.com>
2021-04-26 00:19:31 +0300

    top-bar

--- src/app/auth/services/auth.services.ts
+++ src/app/auth/services/auth.services.ts
@@ -1,21 +1,33 @@
-import {Injectable} from '@angular/core';
-import {RegisterRequestInterface} from '../types/registerRequest.interface';
-import {Observable} from 'rxjs';
-import {CurrentUserInterface} from '../../shared/types/currentUser.interface';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../../../environments/environment';
-import {AuthResponseInterface} from '../../shared/types/authResponse.interface';
-import {map} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { RegisterRequestInterface } from '../types/registerRequest.interface';
+import { Observable } from 'rxjs';
+import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { AuthResponseInterface } from '../../shared/types/authResponse.interface';
+import { map } from 'rxjs/operators';
+import { LoginRequestInterface } from '../types/loginRequest.interface';
 
 @Injectable()
 export class AuthServices {
   constructor(private http: HttpClient) {
   }
 
+  getUser(response: AuthResponseInterface): CurrentUserInterface {
+    return response.user;
+  }
+
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = environment.apiUrl + '/users';
     return this.http.post<AuthResponseInterface>(url, data).pipe(
-      map((response: AuthResponseInterface) => response.user)
+      map(this.getUser)
+    );
+  }
+
+  login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/users/login';
+    return this.http.post<AuthResponseInterface>(url, data).pipe(
+      map(this.getUser)
     );
   }
 }
