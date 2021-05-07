3b60231ea2a716b95267c7a72182d6f01d9affd9
Andrey <a.prudniks@gmail.com>
2021-05-07 10:53:25 +0300

    store for feed

--- src/app/shared/feed/feed.module.ts
+++ src/app/shared/feed/feed.module.ts
@@ -1,11 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FeedComponent} from './components/globalFeed/feed.component';
+import {GetFeedEffect} from './store/effects/getFeed.effect';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
+import {reducers} from './store/reducers';
+import {FeedService} from './services/feed.service';
 
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, EffectsModule.forFeature([GetFeedEffect]), StoreModule.forFeature('feed', reducers)],
   exports: [FeedComponent],
-  declarations: [FeedComponent]
+  declarations: [FeedComponent],
+  providers: [FeedService]
 })
 export class FeedModule {}
